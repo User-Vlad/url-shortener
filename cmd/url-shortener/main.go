bfeb708143b0f047f7cfd98115e83c3013d3e1f5
Vlad <vlad160420035@gmail.com>
2025-02-05 17:24:18 +0700

    add auth to delete

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -49,14 +49,13 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Delete("/{alias}", remove.New(log, storage))
-
-	router.Route("/url", func(r chi.Router) {
+	router.Route("/", func(r chi.Router) {
 		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
 			cfg.HTTPServer.User: cfg.HTTPServer.Password,
 		}))
 
-		r.Post("/", save.New(log, storage))
+		r.Post("/url", save.New(log, storage))
+		r.Delete("/{alias}", remove.New(log, storage))
 	})
 
 	router.Get("/{alias}", redirect.New(log, storage))
